b2e9a978c64c1f903946e16b961ffb88c342f163
kurtn2005 <kurtn@126.com>
2016-10-24 22:58:44 +0800

    Create startRecord.sh
    Start ffmpeg to record multicast streaming, and check the status of multicast, relaunch ffmpeg and record when streaming is recovered.

---
+++ startRecord.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INPUT_PORT=(60002 60012 60022 60032 60042 60052)
+
+while true
+do
+for i in ${INPUT_PORT[@]}
+do
+CHECK=`ps -ef|grep -v "grep"|grep -c "Re_$i"`
+DATE=`date +"%Y-%d-%m_%H:%M:%S"`
+
+if [ $CHECK -eq 0 ]
+then
+#echo "/opt/ffmpeg/ffmpeg -i udp://224.0.0.1:$i?timeout=500 -c copy -f mpegts /v01/Re_${i}_$DATE.ts"
+nohup /opt/ffmpeg/ffmpeg -i udp://224.0.0.1:${i}?timeout=500 -c copy -f mpegts /v01/Re_${i}_$DATE.ts >/dev/null &
+#echo "Restart Re_$i">>/tmp/record.log
+fi
+done
+done
+exit 0

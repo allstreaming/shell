f43c1ce6999351a6eec9c13fa11e9783ad7d9c2b
kurtn2005 <kurtn@126.com>
2021-03-24 11:40:48 +0800

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -2,13 +2,13 @@
 
 DATE=`date +%Y%m%d`
 DATABASES="lb_tb lb_admin lb_cs lb_statis lb_user_login"
-BACKUP_DIR=/home/zyyt/backup
+BACKUP_DIR=/home/PATH/backup
 
 cd $BACKUP_DIR
 
 for i in $DATABASES;
   do
-    /data/application/mysql/bin/mysqldump -hdb.mysql.com -uzyyt -pzyyt@mydb.com -B $i > $i.sql;
+    /data/application/mysql/bin/mysqldump -hHOST -uUSER -pPASSWORD -B $i > $i.sql;
   done
 
 tar jcf DLB-$DATE-W.tar.bz2 *.sql
